16b54d5ca844032164836be2dd691eda94cb7b9d
Ingo Sauerzapf <ingo.sauerzapf@gmail.com>
2022-06-26 07:31:09 +0000

    added set temperature, test still fails

--- tests/test_init.py
+++ tests/test_init.py
@@ -5,7 +5,7 @@ import pytest
 from aiohttp import ClientSession
 from aioresponses import aioresponses
 
-from schluter import InvalidUserPasswordError, SchluterApi
+from schluter import InvalidUserPasswordError, SchluterApi, InvalidSessionIdError
 
 VALID_USERNAME = "valid_user@someplace.org"
 INVALID_USERNAME = "invalid_user@someplace.org"
@@ -99,19 +99,48 @@ async def test_get_current_thermostats():
 
     websession = ClientSession()
     thermostats = {}
+    sessionid = "abcd12345456"
 
     with aioresponses() as session_mock:
         # pylint:disable=line-too-long
         session_mock.get(
-            "https://ditra-heat-e-wifi.schluter.com/api/thermostats?sessionId=abcd12345456",
+            f"https://ditra-heat-e-wifi.schluter.com/api/thermostats?sessionId={sessionid}",
             payload=thermostat_data,
         )
         schluter = SchluterApi(VALID_USERNAME, VALID_PASSWORD, websession)
-        sessionid = "abcd12345456"
         try:
             thermostats = await schluter.async_get_current_thermostats(sessionid)
-        except InvalidUserPasswordError as ex:
-            assert False, f"Raised InvalidUserPasswordError exception {ex}"
+        except InvalidSessionIdError as ex:
+            assert False, f"Raised InvalidSessionIdError exception {ex}"
 
     await websession.close()
     assert thermostats["1084135"].name == "Bathroom"
+
+
+#@pytest.mark.asyncio
+#async def test_success_set_temperature():
+#    """ Test setting the temerature of a specific thermostat"""
+#    with open("tests/fixtures/temperature_set_data.json", encoding="utf-8") as file:
+#        success_data = json.load(file)
+#
+#    websession = ClientSession
+#    sessionid = "abcd12345456"
+#    serialnumber = "abcd12245"
+#    temperature = 22.5
+#
+#    with aioresponses() as session_mock:
+#        #pylint:disable=line-too-long
+#        session_mock.post(
+#            f"https://ditra-heat-e-wifi.schluter.com/api/thermostat?sessionId={sessionid}&serialnumber={serialnumber}",
+#            payload=success_data,
+#        )
+#        schluter = SchluterApi(VALID_USERNAME, VALID_PASSWORD, websession)
+#
+#        try:
+#            res = await schluter.async_set_temperature(sessionid, serialnumber, temperature)
+#        except InvalidSessionIdError as ex:
+#            assert False, f"Raised InvalidSessionIdError exception {ex}"
+#
+#    await websession.close()
+#    assert res is True
+            
